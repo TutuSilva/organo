05eaf6d6ea2aaf8bab44f87f8cd623ae6509af46
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-06-18 08:32:06 -0300

    formulario quase concluido

--- src/componentes/form/index.js
+++ src/componentes/form/index.js
@@ -1,20 +1,46 @@
 import CampoTexto from "../CampoTexto";
 import "./form.css";
+import ListaSuspensa from "./../ListaSuspensa/index";
+import Botao from "../Botão";
 
 const Formu = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "Ux e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("TESTE");
+  };
   return (
     <section className="formulario-container">
-        <form className="formulario">
-          <h2 className="titulo-form">
-            Preencha os dados para criar o card do colaborador.
-          </h2>
-          <CampoTexto label="Nome" placeholder="Digite seu nome" />
-          <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-          <CampoTexto
-            label="Imagem"
-            placeholder="Digite o endereço da sua imagem"
-          />
-        </form>
+      <form onSubmit={aoSalvar} className="formulario">
+        <h2 className="titulo-form">
+          Preencha os dados para criar o card do colaborador.
+        </h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da sua imagem"
+        />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
+      </form>
     </section>
   );
 };
