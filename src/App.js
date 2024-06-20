26b953bb7f6432a3d79a7fd7a81a7dae38346b73
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-06-20 08:53:44 -0300

    time quyase feito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formu from "./componentes/form";
+import Time from "./componentes/Times";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57c278",
+      corSecundaria: "#d9f7e9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82cffa",
+      corSecundaria: "#e8f8ff",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#a6d157",
+      corSecundaria: "#f0f8e2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "",
+      corSecundaria: "",
+    },
+    {
+      nome: "Ux e Design ",
+      corPrimaria: "#db6ebf",
+      corSecundaria: "#fae9f5",
+    },
+    {
+      nome: "Mobile ",
+      corPrimaria: "#ffba05",
+      corSecundaria: "#fff5d9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8a29",
+      corSecundaria: "#ffeedf",
+    },
+  ];
   const [colaboradores, setColaboradores] = useState([]);
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador])
+    setColaboradores([...colaboradores, colaborador]);
   };
   return (
     <div className="App">
       <Banner />
-      <Formu aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Formu
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+      {times.map((time) => (
+        <Time nome={time.nome} />
+      ))}
     </div>
   );
 }
